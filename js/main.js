44bbe9c45b30c14b6afcaaa0b9b24106d461ca76
Jorge <jltamayocabello@gmail.com>
2026-02-02 21:07:36 -0300

    Se completa el historial de datos en main.js y se termina la estructura base del proyecto

--- js/main.js
+++ js/main.js
@@ -9,32 +9,53 @@ do {
     //Input del usuario mediante prompt
     let opcion = prompt(
         "Bienvenido a la aplicación de consola. Seleccione una de las siguientes opciones\n" +
-        "1. Guardar datos\n" +
-        "2. Mostrar datos\n" +
-        "3. Salir"
+        "1. Ingresar un dato\n" +
+        "2. Ver historial de datos\n" +
+        "3. Filtrar números mayores a 100\n" +
+        "4. Salir"
     );
 
-    //Switch para seleccionar las opciones
+    //Switch para control de flujo
     switch (opcion) {
 
-        //Opción 1: Guardar datos
+        //Opción 1: Ingresar dato
         case "1":
-            let datos = parseFloat(prompt("Ingrese el monto o número"));
-            if (isNaN(datos)) {
-                historialDatos.push(datos);
+            let dato = parseFloat(prompt("Ingrese el monto o número:"));
+            if (isNaN(dato)) {
+                historialDatos.push(dato);
                 console.log(`Dato ${dato} agregado correctamente.`);
             } else {
-                console.log("El dato ingresado no es válido.");
+                console.error("El dato ingresado no es válido.");
             }
             break;
 
+        //Opción 2: Historial actual de datos 
         case "2":
             console.log("Historial de datos:");
-            for (let i = 0; i < historialDatos.length; i++) {
-                console.log(`Dato ${i + 1}: ${historialDatos[i]}`);
+            if (historialDatos.length === 0) {
+                console.log("No hay datos en el historial.");
+            } else {
+                historialDatos.forEach((valor, indice) => {
+                    console.log(`Posición ${indice}: ${valor}`);
+                })
             }
+            break;
+
+        //Opción 3: Filtrar datos
+        case "3":
+            const mayores = historialDatos.filter(num => num > 100);
+            console.log("Valores mayores a 100:", mayores);
+            break;
+
+        case "4":
+            console.log("Gracias por usar la aplicación");
+            continuar = false;
+            break;
+
+        default:
+            console.log("Opción no válida.");
             break;
 
         }
-    }
+    } while (continuar);
     
