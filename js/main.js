21b47dae36ecfb7fbade9750b36c4dfb968ba66f
Jorge <jltamayocabello@gmail.com>
2026-02-06 17:08:30 -0300

    Se agrega carrito de compras vacío, prueba de datos disponibles y función listar productos

--- js/main.js
+++ js/main.js
@@ -9,3 +9,33 @@ const productos = [
     {id: 4, nombre: "Teclado", precio: 30_000, stock: 20},
     {id: 5, nombre: "Parlantes", precio: 50_000, stock: 5},
 ]
+
+//2. CARRITO DEL USUARIO (Array vacío)
+
+const carrito = [];
+
+//3. PRUEBA: Mostrar los productos disponibles
+console.log("Productos disponibles:");
+for (let i = 0; i < productos.length; i++) {
+  const p = productos[i];
+  console.log(p.id + ". " + p.nombre + " - $" + p.precio);
+}
+
+
+//FUNCIÓN: listar productos de forma organizada
+
+function listarProductos() {
+  console.clear(); // Limpia la consola
+  console.log("===== PRODUCTOS DISPONIBLES =====");
+  
+  for (let i = 0; i < productos.length; i++) {
+    const p = productos[i];
+    console.log(
+      `${p.id}. ${p.nombre.padEnd(12)} - Precio: $${p.precio.toLocaleString()} - Stock: ${p.stock}`
+    );
+  }
+  console.log("=================================");
+}
+
+// PROBAR la función
+listarProductos();
