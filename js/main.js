5c3f29f39a4ae3c9fb4bee8e7af386f9fd8b859e
Jorge <jltamayocabello@gmail.com>
2026-02-06 16:52:34 -0300

    Se agrega a index.html que la aplicación de consola será un carrito de compras y se agrega el catálogo de productos en main.js

--- js/main.js
+++ js/main.js
@@ -1,69 +1,11 @@
+// CARRITO DE COMPRAS
 
-//HISTORIAL DE DATOS
-const historialDatos = [];
+//1. CATÁLOGO DE PRODUCTOS (Arrays de objetos)
 
-//FUNCIÓN DE INGRESO
-function ingresarDato(){
-    // Input
-    let input = prompt("Ingrese el monto que quiere incorporar: ");
-    // Validación
-    let dato = parseFloat(input);
-    if (!isNaN(dato)) {
-        historialDatos.push(dato);
-    } else {
-        alert("Error, debes ingresar un número");
-    }
-}
-
-//FUNCIÓN DE VISUALIZACIÓN
-function verHistorial(){
-    // Opción 1: Validación de datos
-    if (historialDatos.length === 0) {
-        alert("No hay datos en el historial.");
-        return;
-    }
-
-    // Opción 2: Visualización de datos
-    let mensajeHistorial = "Historial de datos:\n";
-
-    historialDatos.forEach((valor, indice) => {
-        mensajeHistorial += `Posición ${indice}: ${valor}\n`;
-    });
-
-    alert(mensajeHistorial);
-}
-
-// CONTROLADOR PRINCIPAL
-
-function iniciarApp(){}
-let continuar = true;
-
-do {
-    //Input del usuario mediante prompt
-    let opcion = prompt(
-        "Bienvenido a la aplicación de consola. Seleccione una de las siguientes opciones\n" +
-        "1. Ingresar un dato\n" +
-        "2. Ver historial de datos\n" +
-        "4. Salir"
-    );
-
-    //Switch para control de flujo
-    switch (opcion) {
-        case "1":
-            ingresarDato(); 
-            break;
-        case "2":
-            verHistorial();
-            break;
-        case "4":
-            continuar = false;
-            break;
-        default:
-            alert("Opcion no valida");
-            break;
-    }    
-    } while (continuar);
-
-    //EJECUCIÓN
-    iniciarApp();
-    
+const productos = [
+    {id: 1, nombre: "Laptop", precio: 500_000, stock: 5},
+    {id: 2, nombre: "Monitor", precio: 100_000, stock: 10},
+    {id: 3, nombre: "Mouse", precio: 20_000, stock: 15},
+    {id: 4, nombre: "Teclado", precio: 30_000, stock: 20},
+    {id: 5, nombre: "Parlantes", precio: 50_000, stock: 5},
+]
